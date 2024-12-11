7ae5662dfe0d628531ea51456b87475e807e05c0
Meet4009 <meetsolanki4009@gmail.com>
2024-12-11 16:48:30 +0530

    add userschema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 
 const userSchema = new mongoose.Schema({
@@ -6,9 +8,43 @@ const userSchema = new mongoose.Schema({
         firstname: {
             type: String,
             required: true,
-            unique:true,
-            minlength:[3,'First name must be at least 3 character long ']
-
+            unique: true,
+            minlength: [3, 'First name must be at least 3 character long.']
+        },
+        firstname: {
+            type: String,
+            // required: true,
+            unique: true,
+            minlength: [3, 'Last name must be at least 3 character long.']
         }
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [5, 'Email must be at least 5 character long.'],
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+    socketId: {
+        type: String,
     }
 });
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET)
+    return token;
+}
+
+userSchema.methods.comperePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.statics.hashPassword = async function (password){
+    return await bcrypt.hash(password,10)
+}
+
+const userModel = mongoose.model("user",userSchema);
